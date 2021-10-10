99e63bd72666a91dbb19056510a0069fb6a2bbea
Wihan Pretorius <79899697+WihanPre@users.noreply.github.com>
2021-10-10 17:53:50 +0200

    lint

--- src/app/main.py
+++ src/app/main.py
@@ -32,11 +32,11 @@ async def root():
     return {"message": "I am healthy, now what"}
 
 # Basic crud operations
-@app.post("/appointments/{patient_id}/patients/", response_model=schemas.Appointment)
+@app.post("/appointment/{patient_id}/patient/", response_model=schemas.Appointment)
 async def create_appointment(id: int, patient_id: int, appointment: schemas.AppointmentCreate, db: Session = Depends(get_db)):
     return crud.create_patient_appointment(db=db, appointment=appointment, id=id, patient_id=patient_id)
 
-@app.post("/patients/", response_model=schemas.Patient)
+@app.post("/patient/", response_model=schemas.Patient)
 async def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
     db_patient = crud.get_patient_by_name(db, name=patient.name)
     if db_patient:
@@ -48,14 +48,14 @@ async def get_appointments(skip: int = 0, limit: int = 200, db: Session = Depend
     appointments = crud.get_appointments(db=db, skip=skip, limit=limit)
     return appointments
 
-@app.get("/appointments/{id}", response_model=schemas.Appointment)
+@app.get("/appointment/{id}", response_model=schemas.Appointment)
 async def get_appointmet_id(id: int, db: Session = Depends(get_db)):
     appointment = crud.get_appointment_by_id(db, id=id)
     if appointment is None:
         raise HTTPException (status_code=404, detail="Appointment does not exist")  #lookup raise
     return appointment
 
-@app.delete("/appointments/{id}", status_code=200)
+@app.delete("/appointment/{id}", status_code=200)
 async def delete_appointement_by_id(id: int, db: Session = Depends(get_db)):
     db_appointment = crud.get_appointment_by_id(db, id)
     if db_appointment is None:
